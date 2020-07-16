00ce059dc3b9a6c182f42c642d1645aecdf7d6af
marchuanv <marchuanv@gmail.com>
2020-07-16 11:44:30 +0200

    fixed

--- component.messagebus.host.js
+++ component.messagebus.host.js
@@ -7,10 +7,10 @@ module.exports = {
     handle: async (callingModule, { publicHost, publicPort, privateHost, privatePort }) => {
         const thisModule = `component.messagebus.host.${publicHost}.${publicPort}`;
         const hosts = [];
-        delegate.register(thisModule, async ({ headers: {  username, passphrase, channel, publichost, publicport } }) => {
+        delegate.register(thisModule, async ({ headers: {  username, passphrase, channel, publichost, publicport, privatehost, privateport } }) => {
             let message = "";
-            if (!passphrase || !publichost || !publicport || !channel){
-                message = "missing headers: passphrase, publichost, publicport and channel";
+            if (!passphrase || !publichost || !publicport || !channel || !privatehost || !privateport){
+                message = "missing headers: passphrase, publichost, publicport, privatehost, privateport and channel";
                 return { headers: { "Content-Type":"text/plain", "Content-Length": Buffer.byteLength(message) }, statusCode: 400, statusMessage: "Bad Request", data: message };
             }
             if (isNaN(Number(publicport))){
@@ -24,7 +24,7 @@ module.exports = {
                 return { headers: { "Content-Type":"text/plain", "Content-Length": Buffer.byteLength(message) }, statusCode: 400, statusMessage: "Bad Request", data: message };
             }
             const { hashedPassphrase, hashedPassphraseSalt } = utils.hashPassphrase(passphrase);
-            const host = { id: utils.generateGUID(), channel, username, hashedPassphrase, hashedPassphraseSalt, publicHost: publichost, publicPort: publicport };
+            const host = { id: utils.generateGUID(), channel, username, hashedPassphrase, hashedPassphraseSalt, publicHost: publichost, publicPort: publicport,  privateHost: privatehost, privatePort: privateport };
             hosts.push(host)
             logging.write(`MessageBus Host`,`${JSON.stringify(host, null, 4)} registered`);
             return await delegate.call(callingModule, { hosts });
