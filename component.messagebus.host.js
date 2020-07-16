7529a0f8bc73c4c8aec24bcaef96a1a06577fdeb
marchuanv <marchuanv@gmail.com>
2020-07-16 09:58:50 +0200

    fixed

--- component.messagebus.host.js
+++ component.messagebus.host.js
@@ -6,13 +6,42 @@ const requestHandlerSecure = require("component.request.handler.secure");
 const requestSecure = require("component.request.secure");
 
 const logging = require("logging");
-logging.config.add("MessageBusHost");
+logging.config.add("MessageBus Host");
 
 module.exports = { 
     hosts: [],
     handle: async (callingModule, { channel, publicHost, publicPort, privateHost, privatePort }) => {
         const thisModule = `component.messagebus.host.${channel}`;
-        delegate.register(thisModule, async ({ headers: {  username, passphrase, publicHost, publicPort, data } }) => {
+        delegate.register(thisModule, async ({ headers: {  username, passphrase, publichost, publicport, data } }) => {
+
+            let message = "";
+            if (!passphrase){
+                message = "missing headers: passphrase, publichost and publicport";
+                return { 
+                    headers: { 
+                        "Content-Type":"text/plain", 
+                        "Content-Length": Buffer.byteLength(message)
+                    },
+                    statusCode: 400,
+                    statusMessage: "Bad Request",
+                    data: message
+                };
+            }
+
+            const exists = module.exports.hosts.find( h => h.username === username && h.publicHost === publichost && h.publicPort === publicport);
+            if (exists){
+                message = "host already registered";
+                return { 
+                    headers: { 
+                        "Content-Type":"text/plain", 
+                        "Content-Length": Buffer.byteLength(message)
+                    },
+                    statusCode: 400,
+                    statusMessage: "Bad Request",
+                    data: message
+                };
+            }
+
             let { hashedPassphrase, hashedPassphraseSalt } = {};
             if (passphrase){
                 ({ hashedPassphrase, hashedPassphraseSalt } = utils.hashPassphrase(passphrase));
@@ -23,31 +52,12 @@ module.exports = {
                 username,
                 hashedPassphrase,
                 hashedPassphraseSalt,
-                publicHost,
-                publicPort,
-                remote: {
-                    hosts: [data]
-                }
+                publicHost: publichost,
+                publicPort: Number(publicport)
             };
-            logging.write(`MessageBusHost`,`registered`);
-            for(const remoteHost of module.exports.hosts){
-                await requestSecure.send({
-                    host: remoteHost.publicHost,
-                    port: remoteHost.publicPort,
-                    path: `/${remoteHost.channel}/host`,
-                    method: "POST",
-                    headers: {
-                        username: host.username,
-                        hashedPassphrase: remoteHost.hashedPassphrase,
-                        hashedPassphraseSalt: remoteHost.hashedPassphraseSalt,
-                        fromhost: host.publicHost,
-                        fromport: host.publicPort
-                    }, 
-                    data: JSON.stringify(host)
-                });
-            }
-            await delegate.call(callingModule, { host });
+            logging.write(`MessageBus Host`,`${JSON.stringify(host,null,4)} registered`);
             module.exports.hosts.push(host);
+            return await delegate.call(callingModule, { host });
         });
         await requestHandlerSecure.handle(thisModule, { 
             publicHost, 
